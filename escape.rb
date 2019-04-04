071333fe34bb52405302e939f0eb4ef3792ea0ac
Anibal Linares <48930195+anibal85@users.noreply.github.com>
2019-04-04 11:38:20 -0300

    Update escape.rb

--- escape.rb
+++ escape.rb
@@ -1,7 +1,8 @@
 g = ARGV[0].to_i
-# gravedad
+# Ingresa la gravedad
 r = ARGV[1].to_i
-#radio 
+# Ingresa la radio
 total = Math.sqrt(2*g*r)
 # calcular la gravedad y el radio 
 puts "La Velocidad de escape es #{total}" 
+# salida de la Velocidad de escape en total 
